1de0dacb33637ece160504a24305274d4497cf09
Nowfel <amahfuj.cse@gmail.com>
2023-02-27 18:55:34 +0600

    Implement CRUD functionality for Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use App\Http\Controllers\Employee\EmployeeController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +19,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/hello', function () {
-    return response('welcome');
-});
+
+Route::get('/employee', [EmployeeController::class, 'index'])->name('employee');
+Route::get('/name', [EmployeeController::class, 'name']);
+Route::get('/employee/create', [EmployeeController::class, 'create'])->name('employee.create');
+Route::post('/employee/store', [EmployeeController::class, 'store'])->name('employee.store');
+Route::get('/employee/edit/{id}', [EmployeeController::class, 'edit'])->name('employee.edit');
+Route::post('/employee/update/{id}', [EmployeeController::class, 'update'])->name('employee.update');
+Route::get('/employee/delete/{id}', [EmployeeController::class, 'destroy'])->name('employee.delete');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
