9c27b26542ed8ad98de9c5b6e2377ffb8b51d6c9
Nowfel <amahfuj.cse@gmail.com>
2023-03-01 13:01:17 +0600

    Added product search functionality with pdName

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -44,6 +44,9 @@ class ProductController extends Controller
         return Product::destroy($id);
     }
 
-     
+    public function search($name)
+    {
+        return Product::where('name', 'like', '%'.$name.'%')->get();
+    }
    
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
